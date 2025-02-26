d663c5091517473170d476b83ea327203255a582
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-26 23:12:01 +0500

    Update utils.dart

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,15 +1,16 @@
 
 import 'package:flutter/material.dart';
+import 'package:practice_ryde/src/controllers/components/custom_text.dart';
 import 'package:toastification/toastification.dart';
 
 
 class Utils{
 
-  void toastMsg( String msg, BuildContext context)
+  void toastMsg( String msg, BuildContext context, Color color)
   {
     toastification.show(
       context: context,
-      title: Text(msg),
+      title: CustomText(text: msg,color: color),
       autoCloseDuration: const Duration(seconds: 5),
       type: ToastificationType.error,
       backgroundColor: Colors.white,
