6d098bcaed35458db0dcb341f4fe9722e1e8a91b
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-27 19:25:42 +0500

    Update sign_up_view.dart

--- lib/src/view/authviews/signupview/sign_up_view.dart
+++ lib/src/view/authviews/signupview/sign_up_view.dart
@@ -61,32 +61,7 @@ TextEditingController emailController=TextEditingController();
                 loading: loading,
                 onPressed: ()
                 {
-                  setState(() {
-                    loading=true;
-                  });
-                  _auth.createUserWithEmailAndPassword(email: emailController.text.toString(), password: passwordController.text.toString()).then((value){
-                    setState(() {
-                      loading=false;
-                    });
-                    Utils().toastMsg(
-                       "Sign Up Successfully",
-                        context,
-                        Theme.of(context).colorScheme.background,
-                        ToastificationType.success
-                    );
-                     Navigator.push(context,  CupertinoPageRoute(builder: (context)=>SignInView()));
-                  }).onError((e,catcherror){
-                    setState(() {
-                      loading=false;
-                    });
-                   Utils().toastMsg(
-                     e.toString(),
-                     context,
-                       Theme.of(context).colorScheme.background,
-                       ToastificationType.error
-                   );
-                  });
-                 Navigator.push(context,  CupertinoPageRoute(builder: (context)=>SignInView()));
+                 
                 },
                 text: 'Sign Up',
                 color:Theme.of(context).colorScheme.primary,
