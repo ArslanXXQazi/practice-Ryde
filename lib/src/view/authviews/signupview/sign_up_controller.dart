13c274d58ff74a30f4ecf04ffb1f891b2950271c
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-28 22:51:51 +0500

    Update sign_up_controller.dart

--- lib/src/view/authviews/signupview/sign_up_controller.dart
+++ lib/src/view/authviews/signupview/sign_up_controller.dart
@@ -1,4 +1,5 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/get.dart';
@@ -9,7 +10,7 @@ class SignUpController extends GetxController{
   TextEditingController passwordController=TextEditingController();
   TextEditingController emailController=TextEditingController();
   bool loading=false;
-
+  FirebaseAuth _auth=FirebaseAuth.instance;
 
 
 
