d49914e71f7335afd32f8e348ac3cb52e4f0aac1
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-28 19:45:17 +0500

    Update splashscreen.dart

--- lib/src/view/startingviews/splashview/splashscreen.dart
+++ lib/src/view/startingviews/splashview/splashscreen.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:practice_ryde/src/controllers/constants/linker.dart';
 
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -12,34 +11,39 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
-    login();
-    // Future.delayed(Duration(seconds: 3),(){
-    //   Navigator.push(context, CupertinoPageRoute(builder: (context)=>SignInView()));
-    // });
+    Future.delayed(const Duration(seconds: 3), () {
+      login();
+    });
   }
 
-   void login()
- async {
-    User? user=await FirebaseAuth.instance.currentUser;
-    if(user!=null){
-      Navigator.push(context, CupertinoPageRoute(builder: (context)=>NavBarView()));
-    }
-    else{
-      Navigator.push(context, CupertinoPageRoute(builder: (context)=>SignInView()));
+  void login() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (mounted) {
+      if (user != null) {
+        Navigator.pushReplacement(
+            context, CupertinoPageRoute(builder: (context) => NavBarView()));
+      } else {
+        Navigator.pushReplacement(
+            context, CupertinoPageRoute(builder: (context) => SignInView()));
+      }
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
-    final width=MediaQuery.sizeOf(context).width;
-    final height=MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body:Center(child: ImageIcon(AssetImage(Appimages.appLogo),size: width*.3,color: Theme.of(context).colorScheme.secondary,)),
+      body: Center(
+        child: ImageIcon(
+          AssetImage(Appimages.appLogo),
+          size: width * .3,
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
     );
   }
 }
