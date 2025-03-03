5ee91311f3ad8333ce44bd37ef8bb6da15055989
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-03 22:20:08 +0500

    Update sign_up_view.dart

--- lib/src/view/authviews/signupview/sign_up_view.dart
+++ lib/src/view/authviews/signupview/sign_up_view.dart
@@ -1,99 +1,8 @@
-//
-// import 'package:flutter/cupertino.dart';
-// import 'package:practice_ryde/src/controllers/constants/linker.dart';
-//
-//
-//
-// class SignUpView extends StatefulWidget {
-//   SignUpView({super.key});
-//
-//   @override
-//   State<SignUpView> createState() => _SignUpViewState();
-// }
-//
-// class _SignUpViewState extends State<SignUpView> {
-//   SignUpController signupcontroller = Get.put(SignUpController());
-//   final _formField=GlobalKey<FormState> ();
-//   final SignUpController signupController = Get.put(SignUpController());
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final width=MediaQuery.sizeOf(context).width;
-//     final height=MediaQuery.sizeOf(context).height;
-//     return  Scaffold(
-//       backgroundColor: Theme.of(context).colorScheme.background,
-//       body: Padding(
-//         padding:  EdgeInsets.symmetric(vertical: height*.07,horizontal: width*.04),
-//         child: SingleChildScrollView(
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               Center(child: Image(image: AssetImage(Appimages.appLogo),color: Theme.of(context).colorScheme.secondary,)),
-//               Padding(
-//                 padding:  EdgeInsets.symmetric(vertical: height*.02),
-//                 child: BoldText(text: 'Sign Up',color: Theme.of(context).colorScheme.secondary,),
-//               ),
-//             Form(
-//               key: _formField,
-//                 child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//               CustomText(text: 'Your Name',color: Theme.of(context).colorScheme.secondary),
-//               CustomTextFormField(hintText: 'Enter your name', controller: signupcontroller.nameController,msg: "Enter Your Name",),
-//               CustomText(text: 'Email or Phone',color: Theme.of(context).colorScheme.secondary),
-//               CustomTextFormField(hintText: 'Enter your or phone number', controller: signupcontroller.emailController),
-//               CustomText(text: 'Password',color: Theme.of(context).colorScheme.secondary),
-//               PasswordTextFormField(hintText: 'Create a password', controller:signupcontroller.passwordController),
-//             ],)),
-//
-//               Obx(()=>
-//                   CustomButton(
-//                  loading: signupcontroller.loading.value,
-//                 onPressed: () {
-//                   if(_formField.currentState!.validate()){
-//                     signupcontroller.signUp(context);
-//                   }
-//                 },
-//                   text: "Sign Up",
-//                   color:Theme.of(context).colorScheme.primary,
-//                    borderColor: Theme.of(context).colorScheme.primary,
-//
-//               )),
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                 CustomText(text: "Already a member?",color: Theme.of(context).colorScheme.secondary),
-//                 TextButton(
-//                     onPressed: (){
-//                       Navigator.push(context,  CupertinoPageRoute(builder: (context)=>SignInView()));
-//                     },
-//                     child: BoldText(text: 'Log in',fontSize: 16,color: Theme.of(context).colorScheme.secondary),
-//                 ),
-//               ],),
-//               Padding(
-//                 padding:  EdgeInsets.only(bottom: height*.01),
-//                 child: Center(child: BoldText(text: 'Or',fontSize: 20,color: Theme.of(context).colorScheme.secondary)),
-//               ),
-//               SocialButton(
-//                   onPressed: (){},
-//                   image: Appimages.google,
-//                   text: "Continue with Google"
-//               ),
-//               SocialButton(
-//                   onPressed: (){},
-//                   image: Appimages.apple,
-//                   text: "Continue with Apple"
-//               )
-//             ],),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 
 import 'package:flutter/cupertino.dart';
 import 'package:practice_ryde/src/controllers/constants/linker.dart';
+
+
 
 class SignUpView extends StatefulWidget {
   SignUpView({super.key});
@@ -103,101 +12,192 @@ class SignUpView extends StatefulWidget {
 }
 
 class _SignUpViewState extends State<SignUpView> {
-  final _formField = GlobalKey<FormState>();
+  SignUpController signupcontroller = Get.put(SignUpController());
+  final _formField=GlobalKey<FormState> ();
   final SignUpController signupController = Get.put(SignUpController());
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.sizeOf(context).width;
-    final height = MediaQuery.sizeOf(context).height;
-    return Scaffold(
+    final width=MediaQuery.sizeOf(context).width;
+    final height=MediaQuery.sizeOf(context).height;
+    return  Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Padding(
-        padding: EdgeInsets.symmetric(vertical: height * .07, horizontal: width * .04),
+        padding:  EdgeInsets.symmetric(vertical: height*.07,horizontal: width*.04),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Center(
-                child: Image(
-                  image: AssetImage(Appimages.appLogo),
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-              ),
+              Center(child: Image(image: AssetImage(Appimages.appLogo),color: Theme.of(context).colorScheme.secondary,)),
               Padding(
-                padding: EdgeInsets.symmetric(vertical: height * .02),
-                child: BoldText(text: 'Sign Up', color: Theme.of(context).colorScheme.secondary),
+                padding:  EdgeInsets.symmetric(vertical: height*.02),
+                child: BoldText(text: 'Sign Up',color: Theme.of(context).colorScheme.secondary,),
               ),
-              Form(
-                key: _formField,
+            Form(
+              key: _formField,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CustomText(text: 'Your Name', color: Theme.of(context).colorScheme.secondary),
-                    CustomTextFormField(
-                      hintText: 'Enter your name',
-                      controller: signupController.nameController,
-                      msg: "Enter Your Name",
-                    ),
-                    CustomText(text: 'Email', color: Theme.of(context).colorScheme.secondary),
-                    CustomTextFormField(
-                      hintText: 'Enter your email',
-                      controller: signupController.emailController,
-                      msg: "Enter a valid email",
-                    ),
-                    CustomText(text: 'Password', color: Theme.of(context).colorScheme.secondary),
-                    PasswordTextFormField(
-                      hintText: 'Create a password',
-                      controller: signupController.passwordController,
-                    ),
-                  ],
-                ),
-              ),
-              Obx(
-                    () => CustomButton(
-                  loading: signupController.loading.value,
-                  onPressed: () {
-                    if (_formField.currentState!.validate()) {
-                      signupController.signUp(context);
-                    }
-                  },
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              CustomText(text: 'Your Name',color: Theme.of(context).colorScheme.secondary),
+              CustomTextFormField(hintText: 'Enter your name', controller: signupcontroller.nameController,msg: "Enter Your Name",),
+              CustomText(text: 'Email or Phone',color: Theme.of(context).colorScheme.secondary),
+              CustomTextFormField(hintText: 'Enter your or phone number', controller: signupcontroller.emailController),
+              CustomText(text: 'Password',color: Theme.of(context).colorScheme.secondary),
+              PasswordTextFormField(hintText: 'Create a password', controller:signupcontroller.passwordController),
+            ],)),
+
+              Obx(()=>
+                  CustomButton(
+                 loading: signupcontroller.loading.value,
+                onPressed: () {
+                  if(_formField.currentState!.validate()){
+                    signupcontroller.signUp(context);
+                  }
+                },
                   text: "Sign Up",
-                  color: Theme.of(context).colorScheme.primary,
-                  borderColor: Theme.of(context).colorScheme.primary,
-                ),
-              ),
+                  color:Theme.of(context).colorScheme.primary,
+                   borderColor: Theme.of(context).colorScheme.primary,
+
+              )),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  CustomText(text: "Already a member?", color: Theme.of(context).colorScheme.secondary),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(context, CupertinoPageRoute(builder: (context) => SignInView()));
+                CustomText(text: "Already a member?",color: Theme.of(context).colorScheme.secondary),
+                TextButton(
+                    onPressed: (){
+                     Navigator.pop(context);
                     },
-                    child: BoldText(text: 'Log in', fontSize: 16, color: Theme.of(context).colorScheme.secondary),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: height * .01),
-                child: Center(
-                  child: BoldText(text: 'Or', fontSize: 20, color: Theme.of(context).colorScheme.secondary),
+                    child: BoldText(text: 'Log in',fontSize: 16,color: Theme.of(context).colorScheme.secondary),
                 ),
+              ],),
+              Padding(
+                padding:  EdgeInsets.only(bottom: height*.01),
+                child: Center(child: BoldText(text: 'Or',fontSize: 20,color: Theme.of(context).colorScheme.secondary)),
               ),
               SocialButton(
-                onPressed: () {},
-                image: Appimages.google,
-                text: "Continue with Google",
+                  onPressed: (){},
+                  image: Appimages.google,
+                  text: "Continue with Google"
               ),
               SocialButton(
-                onPressed: () {},
-                image: Appimages.apple,
-                text: "Continue with Apple",
-              ),
-            ],
-          ),
+                  onPressed: (){},
+                  image: Appimages.apple,
+                  text: "Continue with Apple"
+              )
+            ],),
         ),
       ),
     );
   }
 }
+
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:practice_ryde/src/controllers/constants/linker.dart';
+//
+// class SignUpView extends StatefulWidget {
+//   SignUpView({super.key});
+//
+//   @override
+//   State<SignUpView> createState() => _SignUpViewState();
+// }
+//
+// class _SignUpViewState extends State<SignUpView> {
+//   final _formField = GlobalKey<FormState>();
+//   final SignUpController signupController = Get.put(SignUpController());
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final width = MediaQuery.sizeOf(context).width;
+//     final height = MediaQuery.sizeOf(context).height;
+//     return Scaffold(
+//       backgroundColor: Theme.of(context).colorScheme.background,
+//       body: Padding(
+//         padding: EdgeInsets.symmetric(vertical: height * .07, horizontal: width * .04),
+//         child: SingleChildScrollView(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Center(
+//                 child: Image(
+//                   image: AssetImage(Appimages.appLogo),
+//                   color: Theme.of(context).colorScheme.secondary,
+//                 ),
+//               ),
+//               Padding(
+//                 padding: EdgeInsets.symmetric(vertical: height * .02),
+//                 child: BoldText(text: 'Sign Up', color: Theme.of(context).colorScheme.secondary),
+//               ),
+//               Form(
+//                 key: _formField,
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     CustomText(text: 'Your Name', color: Theme.of(context).colorScheme.secondary),
+//                     CustomTextFormField(
+//                       hintText: 'Enter your name',
+//                       controller: signupController.nameController,
+//                       msg: "Enter Your Name",
+//                     ),
+//                     CustomText(text: 'Email', color: Theme.of(context).colorScheme.secondary),
+//                     CustomTextFormField(
+//                       hintText: 'Enter your email',
+//                       controller: signupController.emailController,
+//                       msg: "Enter a valid email",
+//                     ),
+//                     CustomText(text: 'Password', color: Theme.of(context).colorScheme.secondary),
+//                     PasswordTextFormField(
+//                       hintText: 'Create a password',
+//                       controller: signupController.passwordController,
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               Obx(
+//                     () => CustomButton(
+//                   loading: signupController.loading.value,
+//                   onPressed: () {
+//                     if (_formField.currentState!.validate()) {
+//                       signupController.signUp(context);
+//                     }
+//                   },
+//                   text: "Sign Up",
+//                   color: Theme.of(context).colorScheme.primary,
+//                   borderColor: Theme.of(context).colorScheme.primary,
+//                 ),
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   CustomText(text: "Already a member?", color: Theme.of(context).colorScheme.secondary),
+//                   TextButton(
+//                     onPressed: () {
+//                       Navigator.push(context, CupertinoPageRoute(builder: (context) => SignInView()));
+//                     },
+//                     child: BoldText(text: 'Log in', fontSize: 16, color: Theme.of(context).colorScheme.secondary),
+//                   ),
+//                 ],
+//               ),
+//               Padding(
+//                 padding: EdgeInsets.only(bottom: height * .01),
+//                 child: Center(
+//                   child: BoldText(text: 'Or', fontSize: 20, color: Theme.of(context).colorScheme.secondary),
+//                 ),
+//               ),
+//               SocialButton(
+//                 onPressed: () {},
+//                 image: Appimages.google,
+//                 text: "Continue with Google",
+//               ),
+//               SocialButton(
+//                 onPressed: () {},
+//                 image: Appimages.apple,
+//                 text: "Continue with Apple",
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
