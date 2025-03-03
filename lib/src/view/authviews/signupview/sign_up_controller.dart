7b1862328db652c609136a0cdd3ed0d53b384627
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-03 21:46:01 +0500

    Update sign_up_controller.dart

--- lib/src/view/authviews/signupview/sign_up_controller.dart
+++ lib/src/view/authviews/signupview/sign_up_controller.dart
@@ -1,44 +1,92 @@
+//
+// import 'package:flutter/cupertino.dart';
+//
+// import '../../../controllers/constants/linker.dart';
+//
+// class SignUpController extends GetxController{
+//
+//   TextEditingController nameController=TextEditingController();
+//   TextEditingController passwordController=TextEditingController();
+//   TextEditingController emailController=TextEditingController();
+//   var loading=false.obs;
+//   FirebaseAuth _auth=FirebaseAuth.instance;
+//
+//   void signUp(BuildContext context) async
+//   {
+//     loading.value=true;
+//     await _auth.createUserWithEmailAndPassword(email: emailController.text.toString(), password: passwordController.text.toString()).then((value){
+//       loading.value=false;
+//       value.user!.updateDisplayName(nameController.text.toString());
+//       Utils().toastMsg(
+//           "Sign Up Successfully",
+//           context,
+//           Theme.of(context).colorScheme.background,
+//           ToastificationType.success
+//       );
+//       nameController.clear();
+//       emailController.clear();
+//       passwordController.clear();
+//       Navigator.push(context,  CupertinoPageRoute(builder: (context)=>SignInView()));
+//     }).onError((e,catcherror){
+//       loading.value=false;
+//       Utils().toastMsg(
+//           e.toString(),
+//           context,
+//           Theme.of(context).colorScheme.background,
+//           ToastificationType.error
+//       );
+//     });
+//   }
+//
+//
+//
+//
+// }
+
 
 import 'package:flutter/cupertino.dart';
-
 import '../../../controllers/constants/linker.dart';
 
-class SignUpController extends GetxController{
+class SignUpController extends GetxController {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  var loading = false.obs;
+  FirebaseAuth _auth = FirebaseAuth.instance;
 
-  TextEditingController nameController=TextEditingController();
-  TextEditingController passwordController=TextEditingController();
-  TextEditingController emailController=TextEditingController();
-  var loading=false.obs;
-  FirebaseAuth _auth=FirebaseAuth.instance;
+  void signUp(BuildContext context) async {
+    loading.value = true;
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
 
-  void signUp(BuildContext context) async
-  {
-    loading.value=true;
-    await _auth.createUserWithEmailAndPassword(email: emailController.text.toString(), password: passwordController.text.toString()).then((value){
-      loading.value=false;
-      value.user!.updateDisplayName(nameController.text.toString());
-      Utils().toastMsg(
-          "Sign Up Successfully",
+      User? user = userCredential.user;
+      if (user != null) {
+        await user.updateDisplayName(nameController.text.trim());
+        await user.sendEmailVerification(); // Send verification email
+
+        Utils().toastMsg(
+          "Verification email sent. Please verify your email.",
           context,
           Theme.of(context).colorScheme.background,
-          ToastificationType.success
-      );
-      nameController.clear();
-      emailController.clear();
-      passwordController.clear();
-      Navigator.push(context,  CupertinoPageRoute(builder: (context)=>SignInView()));
-    }).onError((e,catcherror){
-      loading.value=false;
+          ToastificationType.success,
+        );
+
+        nameController.clear();
+        emailController.clear();
+        passwordController.clear();
+      }
+    } catch (e) {
       Utils().toastMsg(
-          e.toString(),
-          context,
-          Theme.of(context).colorScheme.background,
-          ToastificationType.error
+        e.toString(),
+        context,
+        Theme.of(context).colorScheme.background,
+        ToastificationType.error,
       );
-    });
+    } finally {
+      loading.value = false;
+    }
   }
-
-
-
-
 }
