02d976d3df0ac10e30fc94b6ff393fc333a4583c
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-04 21:41:04 +0500

    Update sign_up_controller.dart

--- lib/src/view/authviews/signupview/sign_up_controller.dart
+++ lib/src/view/authviews/signupview/sign_up_controller.dart
@@ -2,46 +2,6 @@
 import 'package:flutter/cupertino.dart';
 
 import '../../../controllers/constants/linker.dart';
-
-// class SignUpController extends GetxController{
-//
-//   TextEditingController nameController=TextEditingController();
-//   TextEditingController passwordController=TextEditingController();
-//   TextEditingController emailController=TextEditingController();
-//   var loading=false.obs;
-//   FirebaseAuth _auth=FirebaseAuth.instance;
-//
-//   void signUp(BuildContext context) async
-//   {
-//     loading.value=true;
-//   UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-//       email: emailController.text.toString(),
-//       password: passwordController.text.toString()).then((value){
-//       loading.value=false;
-//       value.user!.updateDisplayName(nameController.text.toString());
-//       Utils().toastMsg(
-//           "Sign Up Successfully",
-//           context,
-//           Theme.of(context).colorScheme.background,
-//           ToastificationType.success
-//       );
-//       nameController.clear();
-//       emailController.clear();
-//       passwordController.clear();
-//       Navigator.push(context,  CupertinoPageRoute(builder: (context)=>SignInView()));
-//     }).onError((e,catcherror){
-//       loading.value=false;
-//       Utils().toastMsg(
-//           e.toString(),
-//           context,
-//           Theme.of(context).colorScheme.background,
-//           ToastificationType.error
-//       );
-//     });
-//   }
-//}
-
-
 class SignUpController extends GetxController{
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
