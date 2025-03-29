af7614b25cb59e1ff52981c0206f3b31a2419e04
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-29 22:43:48 +0500

    Update edit_profile_controller.dart

--- lib/src/view/navbar_views/profile_nav_view/edit_profile_view/edit_profile_controller.dart
+++ lib/src/view/navbar_views/profile_nav_view/edit_profile_view/edit_profile_controller.dart
@@ -10,7 +10,6 @@ import '../../../../controllers/constants/linker.dart';
 import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:dio/dio.dart' as dio;
